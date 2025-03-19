83a15b48600f4d94c4e19ce0ebdfe19d3938683e
Mohammad Jabir <mohammadj94@gmail.com>
2025-03-19 02:49:08 -0600

    initial commit.

---
+++ 34_BetterRandom/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _34_BetterRandom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
